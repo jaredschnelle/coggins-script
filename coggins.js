507d2eb1126920018216cc6e71b76a9b1abfa37a
Jared Schnelle <jared@gameyeti.com>
2022-08-10 14:30:44 -0400

    asdf

--- coggins.js
+++ coggins.js
@@ -1,67 +1,3 @@
-waitForKeyElements = (
-    selectorTxt,    // Required: The jQuery selector string that specifies the desired element(s).
-    actionFunction, // Required: The code to run when elements are found. It is passed a jNode to the matched element.
-    doWaitOnce,      // Optional: If false, will continue to scan for new elements even after the first match is found.
-    iframeSelector  // Optional: If set, identifies the iframe to search.
-) => {
-    var targetNodes, didFindTargets; 
-
-    if (typeof iframeSelector == "undefined") {
-        targetNodes = $(selectorTxt);
-    }
-    else {
-        targetNodes = $(iframeSelector)
-                        .contents()
-                        .find(selectorTxt); 
-    }
-
-   if (targetNodes  &&  targetNodes.length > 0) {
-        didFindTargets = true;
-        
-        // Found target node(s).  Go through each and act if they are new.
-        targetNodes.each ( function () {
-            var $this = $(this);
-            var alreadyFound = $this.data('alreadyFound') || false; 
-
-            if (!alreadyFound) {
-                //--- Call the payload function.
-                var cancelFound = actionFunction($this);
-
-                if (cancelFound)
-                    didFindTargets = false;
-                else
-                    $this.data ('alreadyFound', true);
-            }
-        } );
-    }
-    else {
-        didFindTargets   = false;
-    }
-    
-    //--- Get the timer-control variable for this selector.
-    var controlObj = waitForKeyElements.controlObj || {};
-    var controlKey = selectorTxt.replace(/[^\w]/g, "_");
-    var timeControl = controlObj[controlKey]; 
-
-    //--- Now set or clear the timer as appropriate.
-    if (didFindTargets && doWaitOnce && timeControl) {
-        //--- The only condition where we need to clear the timer.
-        clearInterval(timeControl);
-        delete controlObj[controlKey]
-    }
-    else {
-        //--- Set a timer, if needed.
-        if (!timeControl) {
-            timeControl = setInterval (function () {
-                    waitForKeyElements(selectorTxt, actionFunction, doWaitOnce, iframeSelector);
-                },300);
-
-            controlObj[controlKey] = timeControl;
-        }
-    }
-    waitForKeyElements.controlObj = controlObj;
-}
-
 window.addCogginsLinks = () => {
     $('#chart_wrapper .dataTable tr').each(function(){
         if($(this).find('td').length === 0)
@@ -108,7 +44,7 @@ window.openTestRecordPdf = (recordId, mode) => {
 };
 
 window.runCogginsScript = () => { 
-    window.waitForKeyElements('form[name=findTestChartForm]', function() {
+    waitForKeyElements('form[name=findTestChartForm]', function() {
         var urlParams = new URLSearchParams(window.location.search);
 
         $('#chart').on('draw.dt', function() {
@@ -118,19 +54,15 @@ window.runCogginsScript = () => {
         });
     }, false);
 
-    window.waitForKeyElements('#confirmdata', function() {
+    waitForKeyElements('#confirmdata', function() {
         $('#confirmdata').prop("checked", true);
     }, false);
 
- 
-
-    window.waitForKeyElements('form[name=labSetupAnimalForm]', function() {
+    waitForKeyElements('form[name=labSetupAnimalForm]', function() {
         $('#listAction').val('action.add');
     }, false);
 
- 
-
-    window.waitForKeyElements('form[name=testRecordForm]', function() {
+    .waitForKeyElements('form[name=testRecordForm]', function() {
         var urlParams = new URLSearchParams(window.location.search);
         var quickPdfOpen = urlParams.has('quickopen');
 
@@ -168,4 +100,4 @@ window.runCogginsScript = () => {
             }
         }
     }, false);
-}
+};
